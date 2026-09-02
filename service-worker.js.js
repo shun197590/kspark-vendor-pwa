33b675f48156eb235be2f54b45fe86cac7d5647a
shun197590 <a8156297@gmail.com>
2026-09-02 09:07:23 +0800

    Delete service-worker.js.js

--- service-worker.js.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='kspark-vendor-pwa-v27.0';
-const APP_SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{const r=e.request;if(r.method!=='GET')return;const u=new URL(r.url);if(u.hostname==='script.google.com'||u.hostname==='script.googleusercontent.com')return;if(r.mode==='navigate'){e.respondWith(fetch(r).then(resp=>{const copy=resp.clone();caches.open(CACHE_NAME).then(c=>c.put('./index.html',copy));return resp}).catch(async()=>await caches.match(r)||await caches.match('./index.html')||await caches.match('./')));return}if(u.origin===self.location.origin){e.respondWith(caches.match(r).then(cached=>cached||fetch(r).then(resp=>{if(resp&&resp.status===200){const copy=resp.clone();caches.open(CACHE_NAME).then(c=>c.put(r,copy))}return resp})))}});
